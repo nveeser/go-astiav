1a1ca4c406d36f80d73969b491ef18629ea3f33b
Nicholas <nveeser@gmail.com>
2025-04-15 01:01:21 -0700

    ProbeInputFormat: Add a ProbeInputFormat() and accesors for Codec() OutputFormats and all InputFormat instances
    Add AllInputFormats() AllOutputFormats() to iterate over formats.
    Add Probe to probe the input formats and return the score to determine the type of file
    Add tests for Codecs() and AllOutputFormats() and AllInputFormats()
    
    Add a function to probe the input formats and return the score to determine the type of file

--- input_format_test.go
+++ input_format_test.go
@@ -1,10 +1,23 @@
 package astiav
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func ExampleAllInputFormats() {
+	for _, x := range AllInputFormats() {
+		fmt.Printf("InputFormat: %s\n", x.Name())
+	}
+}
+func TestProbeInputFormat(t *testing.T) {
+	format, score, err := ProbeInputFormat("testdata/video.mp4")
+	require.Nil(t, err)
+	require.Greater(t, score, 0)
+	require.NotNil(t, format)
+}
 
 func TestInputFormat(t *testing.T) {
 	formatName := "rawvideo"
@@ -13,4 +26,7 @@ func TestInputFormat(t *testing.T) {
 	require.Equal(t, formatName, inputFormat.Name())
 	require.Equal(t, formatName, inputFormat.String())
 	require.Equal(t, "raw video", inputFormat.LongName())
+	t.Run("AllInputFormats()", func(t *testing.T) {
+		require.GreaterOrEqual(t, len(AllInputFormats()), 10)
+	})
 }
--- output_format_test.go
+++ output_format_test.go
@@ -1,10 +1,17 @@
 package astiav
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func ExampleAllOutputFormats() {
+	for _, x := range AllOutputFormats() {
+		fmt.Printf("OutputFormat: %s\n", x.Name())
+	}
+}
 
 func TestOutputFormat(t *testing.T) {
 	formatName := "rawvideo"
@@ -14,4 +21,7 @@ func TestOutputFormat(t *testing.T) {
 	require.Equal(t, formatName, outputFormat.String())
 	require.Equal(t, "raw video", outputFormat.LongName())
 	require.Equal(t, []string{"yuv", "rgb"}, outputFormat.Extensions())
+	t.Run("AllOutputFormats()", func(t *testing.T) {
+		require.GreaterOrEqual(t, len(AllOutputFormats()), 10)
+	})
 }
